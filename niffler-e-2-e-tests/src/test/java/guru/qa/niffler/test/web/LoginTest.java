a0bc4f4d0f52ff7803f2d8ab44bf8de89d69cd96
Загорский Владислав <vvzagorski@mts.ru>
2025-05-19 16:10:37 +0300

    [TASK ####] Homework 10.2.
    * added dtuch's code.

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -1,9 +1,11 @@
 package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideDriver;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.utils.SelenideUtils;
 import org.junit.jupiter.api.Test;
 
 import static guru.qa.niffler.utils.RandomDataUtils.randomUsername;
@@ -11,19 +13,25 @@ import static guru.qa.niffler.utils.RandomDataUtils.randomUsername;
 @WebTest
 public class LoginTest {
 
-  private static final Config CFG = Config.getInstance();
+    private static final Config CFG = Config.getInstance();
+    private final SelenideDriver driverChrome = new SelenideDriver(SelenideUtils.chromeConfig);
 
-  @Test
-  void mainPageShouldBeDisplayedAfterSuccessLogin() {
-    Selenide.open(CFG.frontUrl(), LoginPage.class)
-        .successLogin("duck", "12345")
-        .checkThatPageLoaded();
-  }
+    @Test
+    void mainPageShouldBeDisplayedAfterSuccessLogin() {
+        SelenideDriver firefox = new SelenideDriver(SelenideUtils.firefoxConfig);
 
-  @Test
-  void userShouldStayOnLoginPageAfterLoginWithBadCredentials() {
-    LoginPage loginPage = Selenide.open(CFG.frontUrl(), LoginPage.class);
-    loginPage.login(randomUsername(), "BAD");
-    loginPage.checkError("Bad credentials");
-  }
+        driverChrome.open(CFG.frontUrl());
+        firefox.open(CFG.frontUrl());
+
+        new LoginPage(driverChrome)
+                .successLogin("duck", "12345")
+                .checkThatPageLoaded();
+    }
+
+    @Test
+    void userShouldStayOnLoginPageAfterLoginWithBadCredentials() {
+        LoginPage loginPage = Selenide.open(CFG.frontUrl(), LoginPage.class);
+        loginPage.login(randomUsername(), "BAD");
+        loginPage.checkError("Bad credentials");
+    }
 }
